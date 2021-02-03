490ad2808f0804f883d59e01a6c48f55f6590b3d
sathish1802 <sathish.k.gowda@gmail.com>
2021-02-03 15:27:35 +0530

    Initial commit

---
+++ src/main/java/com/sathish/spring/cloud/stream/consumer/api/SpringCloudStreamConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.sathish.spring.cloud.stream.consumer.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStreamConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudStreamConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sathish/spring/cloud/stream/consumer/api/SpringCloudStreamConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sathish.spring.cloud.stream.consumer.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudStreamConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
